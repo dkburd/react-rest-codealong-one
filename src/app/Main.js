a44d253274967b26449d86846434e6065197ecbc
Dana Burd <burd.dana@gmail.com>
2021-05-16 17:21:43 -0700

    var

---
+++ src/app/Main.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import ReactDOM from 'react-ReactDOM'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is our app</h1>
+      <p>blue</p>
+         </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
